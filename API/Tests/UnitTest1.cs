7907a82df6e43ab68c9f1a66c906148db2d36421
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-10-19 15:46:10 -0300

    create conditional to run test to create image only in prod

---
+++ API/Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SuccessTestCase()
+        {
+           Assert.Pass();
+        }
+
+        // [Test]
+        // public void FailTestCase()
+        // {
+        //     Assert.Fail();
+        // }
+    }
+}
