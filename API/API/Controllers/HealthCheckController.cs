3575d819195b97e50317436b5f0a464fefcbc65e
Luiz-Kohler <luizfelipekohler03@gmail.com>
2024-10-19 00:55:32 -0300

    creating api w different env configs

---
+++ API/API/Controllers/HealthCheckController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : Controller
+    {
+        [HttpGet]
+        public IActionResult IsOk()
+        {
+            return Ok(new { status = "^_-" });
+        }
+
+        [HttpGet("GetEnviroment")]
+        public IActionResult GetEnviroment()
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            return Ok(new { env });
+        }
+    }
+}
